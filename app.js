44c1a5881cd6265931901207c4359ab326dca390
mathuramg <mmg9291@gmail.com>
2016-06-10 17:51:30 -0400

    made codemirror editor from textarea

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 var isLightTheme = 1;
+var isTextArea = 1;
 
-var myCodeMirror = CodeMirror(document.body, {
-  value: "console.log('potato')\n",
-  mode:  "javascript",
-  lineNumbers: true,
-  theme: "3024-day",
-  autoCloseBrackets: true,
-  inputStyle: "contenteditable"
-});
+var myCodeMirror;
 
 // let's save this as a globally accessible variable in case we need it for more custom style functions
-var cmWrapperElement = myCodeMirror.getWrapperElement();
+var cmWrapperElement;
 
 // debugger;
-
-codeArea =document.getElementsByClassName('CodeMirror')[0];
-codeArea.addEventListener("keyup", function() {
-  // console.log('changing the value');
-  eval(myCodeMirror.doc.getValue());
-})
 window.onload = function(){
-  
+
+  myCodeMirror = CodeMirror.fromTextArea(document.getElementById('codemirror-test'), {
+    value: "console.log('potato')\n",
+    mode:  "javascript",
+    lineNumbers: true,
+    theme: "3024-day",
+    autoCloseBrackets: true,
+    inputStyle: "contenteditable"
+  });
+
+  isTextArea = 0;
+  cmWrapperElement = myCodeMirror.getWrapperElement();
+
+  codeArea =document.getElementsByClassName('CodeMirror')[0];
+  codeArea.addEventListener("keyup", function() {
+    console.log('changing the value');
+    eval(myCodeMirror.doc.getValue());
+    // eval(textAreaCM.doc.getValue());
+  })
+
   setIDETheme();
 
   //add content editable to the parent class - 'CodeMirror-lines'
@@ -57,4 +64,24 @@ function setFontSize() {
 function setIDETheme() {
   var selTheme = document.getElementById('ideTheme').value;
   myCodeMirror.setOption('theme',selTheme);
+}
+
+function switchToText() {
+  if(isTextArea === 0 ){
+    console.log('switch to text area');
+    myCodeMirror.toTextArea();
+    isTextArea = 1;
+  }
+  else {
+    console.log('switch to text area');
+    myCodeMirror = CodeMirror.fromTextArea(document.getElementById('codemirror-test'), {
+      mode:  "javascript",
+      lineNumbers: true,
+      theme: document.getElementById('ideTheme').value,
+      autoCloseBrackets: true,
+      inputStyle: "contenteditable"
+    });
+    isTextArea = 0;
+  }
+
 }
